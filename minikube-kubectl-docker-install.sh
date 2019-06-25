a44e1316a1baeb38f087582de2c51c67c70d1636
Vijendra Kumar S K <skvijay007@gmail.com>
2019-06-25 14:35:16 +1000

    Create minikube-kubectl-docker-install.sh
    Minikube, Kubectl and Docker installation for local Mac OS

---
+++ minikube-kubectl-docker-install.sh
@@ -0,0 +1,18 @@
+#Install KubeCTL
+sudo apt-get update && sudo apt-get install -y apt-transport-https 
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
+sudo touch /etc/apt/sources.list.d/kubernetes.list 
+
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get update
+sudo apt-get install -y kubectl
+
+#Install MiniKube:
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.28.2/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
+
+#Install Docker:
+sudo apt-get update \
+	&& sudo apt-get install -qy docker.io
